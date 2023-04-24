5b206ffce4ea9001a56bfe61527cad59f9181560
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 16:39:04 -0400

    Update pdf.r

--- pdf.r
+++ pdf.r
@@ -3,13 +3,10 @@ library(jsonlite)
 # Load the JSON object from a file
 data <- fromJSON("data.json")
 
-# Convert the list to a data frame with row names
-df <- as.data.frame(data, row.names = paste0("row", seq_along(data)))
-
-# Check for duplicate row names and add prefix if necessary
-if (any(duplicated(row.names(df)))) {
-  row.names(df) <- paste0("row", seq_along(data))
-}
+# Convert the list to a data frame with an index column
+df <- as.data.frame(data)
+df$index <- 1:nrow(df)
+rownames(df) <- paste0("row", df$index)
 
 # Open a connection to the output PDF file
 pdf("output.pdf")
