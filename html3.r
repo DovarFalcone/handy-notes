781779721b2e13532e591e3c13bc34a39e2587b2
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 18:01:46 -0400

    Update html3.r

--- html3.r
+++ html3.r
@@ -6,75 +6,66 @@ library(tidyr)
 library(knitr)
 library(kableExtra)
 
-# Define input file path
+# Define input and output file paths
 input_path <- "path/to/your/file.csv"
+output_folder <- "path/to/output/folder/"
 
 # Read in the CSV file
 tryCatch({
   csv_data <- read.csv(input_path, stringsAsFactors = FALSE)
 }, error = function(e) {
-  stop("Failed to read CSV file: ", e$message)
+  stop("Failed to read CSV data: ", e$message)
 })
 
-# Check if the CSV file contains a "json_data" column
-if (!"json_data" %in% colnames(csv_data)) {
-  stop("CSV file does not contain a 'json_data' column.")
+# Check if the input data is a data frame
+if (!is.data.frame(csv_data)) {
+  stop("CSV data is not a data frame.")
 }
 
-# Convert the "json_data" column from JSON to a list
-json_list <- tryCatch({
-  lapply(csv_data$json_data, fromJSON, simplifyDataFrame = FALSE)
-}, error = function(e) {
-  stop("Failed to convert JSON data to list: ", e$message)
-})
-
-# Flatten the JSON data into a data frame for each row
-kable_data_list <- tryCatch({
-  map2(json_list, csv_data$facility, function(json, facility) {
-    flat_data <- json %>%
-      as.data.frame(recursive = TRUE) %>%
+# Loop through each row in the data frame
+for (i in 1:nrow(csv_data)) {
+  row <- csv_data[i, ]
+  
+  # Convert the JSON string to a list
+  tryCatch({
+    json_data <- fromJSON(row$json_column, simplifyDataFrame = FALSE)
+  }, error = function(e) {
+    stop(paste0("Failed to read JSON data in row ", i, ": ", e$message))
+  })
+  
+  # Flatten the JSON data into a data frame
+  flat_data <- tryCatch({
+    as.data.frame(json_data, recursive = TRUE) %>%
       gather(key, value) %>%
       unnest(value) %>%
-      select(-key) %>%
       mutate(key = gsub("\\.", " ", key),
              key = tolower(key),
              key = tools::toTitleCase(key)) %>%
+      select(key, value) %>%
       distinct()
-
-    # Convert the data frame to a knitr kable object
-    kable_data <- kable(flat_data, "html", row.names = FALSE) %>%
+  }, error = function(e) {
+    stop(paste0("Failed to flatten JSON data in row ", i, ": ", e$message))
+  })
+  
+  # Check if the flattened data frame is empty
+  if (nrow(flat_data) == 0) {
+    stop(paste0("No data found in JSON object in row ", i, "."))
+  }
+  
+  # Convert the data frame to a knitr kable object
+  kable_data <- tryCatch({
+    kable(flat_data, "html", row.names = FALSE) %>%
       kable_styling("striped", full_width = FALSE) %>%
       column_spec(1, bold = TRUE, background = "#F2F2F2")
-
-    # Write the kable object to an HTML file
-    output_path <- paste0("path/to/output/", gsub(" ", "_", facility), ".html")
-    cat(kable_data, file = output_path)
-
-    # Return the kable object for use in later steps
-    kable_data
+  }, error = function(e) {
+    stop(paste0("Failed to convert data in row ", i, " to kable format: ", e$message))
   })
-}, error = function(e) {
-  stop("Failed to flatten JSON data: ", e$message)
-})
-
-# Check if any of the kable data lists are empty
-if (any(map_int(kable_data_list, nrow) == 0)) {
-  stop("No data found in one or more rows of CSV file.")
+  
+  # Write the kable object to an HTML file
+  tryCatch({
+    output_file <- paste0(output_folder, "output_", i, ".html")
+    cat(kable_data, file = output_file)
+  }, error = function(e) {
+    stop(paste0("Failed to write HTML file for row ", i, ": ", e$message))
+  })
 }
-
-# Combine the kable data lists into a single HTML string
-html_str <- tryCatch({
-  reduce(kable_data_list, function(x, y) {
-    paste(x, y, sep = "\n")
-  })
-}, error = function(e) {
-  stop("Failed to combine kable data: ", e$message)
-})
-
-# Write the combined HTML string to a file
-output_path <- "path/to/output/combined.html"
-tryCatch({
-  cat(html_str, file = output_path)
-}, error = function(e) {
-  stop("Failed to write combined HTML file: ", e$message)
-})
