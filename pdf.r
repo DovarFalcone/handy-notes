18e5786c33823e4287ac1bbc09616aea1e3c130e
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 16:31:12 -0400

    Update pdf.r

--- pdf.r
+++ pdf.r
@@ -1,12 +1,21 @@
 library(jsonlite)
-library(Rcpp)
 
+# Load the JSON object from a file
 data <- fromJSON("data.json")
 
+# Convert the list to a data frame
+df <- as.data.frame(data)
+
+# Open a connection to the output PDF file
 pdf("output.pdf")
 
-for (key in names(data)) {
-  cat(key, ": ", data[[key]], "\n")
+# Print the contents of the data frame to the PDF file
+for (i in seq_len(nrow(df))) {
+  for (j in seq_len(ncol(df))) {
+    cat(paste(names(df)[j], ": ", df[i, j], "\n"))
+  }
+  cat("\n")
 }
 
+# Close the connection to the PDF file
 dev.off()
