243feed171fcd61d1a567e34ed44fd9f175c2389
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 18:46:11 -0400

    Update json.r

--- json.r
+++ json.r
@@ -8,11 +8,20 @@ for (i in 1:nrow(data)) {
   facility_name <- data[i, "facility_name"]
   clean_name <- data[i, "clean_name"]
   time <- data[i, "time"]
-  json_data <- fromJSON(data[i, "json_data"])
+  json_str <- data[i, "json_data"]
+  
+  # Skip over rows with invalid JSON data
+  if (!jsonlite::validate(json_str)) {
+    warning(paste0("Skipping row ", i, " due to invalid JSON data."))
+    next
+  }
+  
+  # Parse the JSON data into a JSON object
+  json_data <- jsonlite::fromJSON(json_str)
   
   # Create a filename
   filename <- paste0(facility_name, "_", clean_name, "_", time, ".json")
   
   # Write the JSON file
-  write_json(json_data, filename)
+  jsonlite::write_json(json_data, filename)
 }
