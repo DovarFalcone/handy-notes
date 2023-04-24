ee23f960fdecb0c959b690b9df47a1875b47a656
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 16:53:51 -0400

    Update pdf.r

--- pdf.r
+++ pdf.r
@@ -6,19 +6,49 @@ library(tidyr)
 library(knitr)
 library(kableExtra)
 
+# Define input and output file paths
+input_path <- "path/to/your/file.json"
+output_path <- "path/to/output/file.pdf"
+
 # Read in the JSON file
-json_data <- fromJSON("path/to/your/file.json")
+tryCatch({
+  json_data <- fromJSON(input_path, simplifyDataFrame = FALSE)
+}, error = function(e) {
+  stop("Failed to read JSON data: ", e$message)
+})
+
+# Check if the input data is a list
+if (!is.list(json_data)) {
+  stop("JSON data is not a list.")
+}
 
 # Flatten the JSON data into a data frame
-flat_data <- as.data.frame(json_data, recursive = TRUE) %>%
-  gather(key, value) %>%
-  unnest(value) %>%
-  select(-key)
+flat_data <- tryCatch({
+  as.data.frame(json_data, recursive = TRUE) %>%
+    gather(key, value) %>%
+    unnest(value) %>%
+    select(-key)
+}, error = function(e) {
+  stop("Failed to flatten JSON data: ", e$message)
+})
+
+# Check if the flattened data frame is empty
+if (nrow(flat_data) == 0) {
+  stop("No data found in JSON file.")
+}
 
 # Convert the data frame to a knitr kable object
-kable_data <- kable(flat_data, "latex", booktabs = TRUE)
+kable_data <- tryCatch({
+  kable(flat_data, "latex", booktabs = TRUE)
+}, error = function(e) {
+  stop("Failed to convert data to kable format: ", e$message)
+})
 
 # Write the kable object to a PDF file
-knitr::kable_styling(kable_data, full_width = FALSE) %>%
-  as.character() %>%
-  cat(file = "path/to/output/file.pdf", sep = "\n")
+tryCatch({
+  knitr::kable_styling(kable_data, full_width = FALSE) %>%
+    as.character() %>%
+    cat(file = output_path, sep = "\n")
+}, error = function(e) {
+  stop("Failed to write PDF file: ", e$message)
+})
