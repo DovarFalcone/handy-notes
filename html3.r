16877b39cf080a80a98514207317bdc14ba5b4f2
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-04-24 17:52:02 -0400

    Update html3.r

--- html3.r
+++ html3.r
@@ -6,9 +6,8 @@ library(tidyr)
 library(knitr)
 library(kableExtra)
 
-# Define input and output file paths
+# Define input file path
 input_path <- "path/to/your/file.csv"
-output_path <- "path/to/output/file.html"
 
 # Read in the CSV file
 tryCatch({
@@ -29,38 +28,53 @@ json_list <- tryCatch({
   stop("Failed to convert JSON data to list: ", e$message)
 })
 
-# Flatten the JSON data into a data frame
-flat_data <- tryCatch({
-  json_df <- bind_rows(lapply(json_list, as.data.frame, recursive = TRUE), .id = "id")
-  json_df %>%
-    gather(key, value, -id) %>%
-    unnest(value) %>%
-    mutate(key = gsub("\\.", " ", key),
-           key = tolower(key),
-           key = tools::toTitleCase(key)) %>%
-    select(id, key, value) %>%
-    distinct()
+# Flatten the JSON data into a data frame for each row
+kable_data_list <- tryCatch({
+  map2(json_list, csv_data$facility, function(json, facility) {
+    flat_data <- json %>%
+      as.data.frame(recursive = TRUE) %>%
+      gather(key, value) %>%
+      unnest(value) %>%
+      select(-key) %>%
+      mutate(key = gsub("\\.", " ", key),
+             key = tolower(key),
+             key = tools::toTitleCase(key)) %>%
+      distinct()
+
+    # Convert the data frame to a knitr kable object
+    kable_data <- kable(flat_data, "html", row.names = FALSE) %>%
+      kable_styling("striped", full_width = FALSE) %>%
+      column_spec(1, bold = TRUE, background = "#F2F2F2")
+
+    # Write the kable object to an HTML file
+    output_path <- paste0("path/to/output/", gsub(" ", "_", facility), ".html")
+    cat(kable_data, file = output_path)
+
+    # Return the kable object for use in later steps
+    kable_data
+  })
 }, error = function(e) {
   stop("Failed to flatten JSON data: ", e$message)
 })
 
-# Check if the flattened data frame is empty
-if (nrow(flat_data) == 0) {
-  stop("No data found in CSV file.")
+# Check if any of the kable data lists are empty
+if (any(map_int(kable_data_list, nrow) == 0)) {
+  stop("No data found in one or more rows of CSV file.")
 }
 
-# Convert the data frame to a knitr kable object
-kable_data <- tryCatch({
-  kable(flat_data, "html", row.names = FALSE) %>%
-    kable_styling("striped", full_width = FALSE) %>%
-    column_spec(1, bold = TRUE, background = "#F2F2F2")
+# Combine the kable data lists into a single HTML string
+html_str <- tryCatch({
+  reduce(kable_data_list, function(x, y) {
+    paste(x, y, sep = "\n")
+  })
 }, error = function(e) {
-  stop("Failed to convert data to kable format: ", e$message)
+  stop("Failed to combine kable data: ", e$message)
 })
 
-# Write the kable object to an HTML file
+# Write the combined HTML string to a file
+output_path <- "path/to/output/combined.html"
 tryCatch({
-  cat(kable_data, file = output_path)
+  cat(html_str, file = output_path)
 }, error = function(e) {
-  stop("Failed to write HTML file: ", e$message)
+  stop("Failed to write combined HTML file: ", e$message)
 })
