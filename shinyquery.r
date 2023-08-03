904897f2f785eac8eb13f41660c623baa5adef74
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-03 10:15:05 -0400

    Update shinyquery.r

--- shinyquery.r
+++ shinyquery.r
@@ -1,33 +1,56 @@
-library(shiny)
-library(shinydashboard)
+## global.r
+# Load required libraries
 library(RPostgreSQL)
 
-# Database connection parameters
+# Database connection parameters (common for all servers)
 db_params <- list(
   dbname = "your_db_name",
-  host = "your_host",
+  host = "",  # The specific server name will be provided later
   port = 5432,  # Your port number, default is 5432 for PostgreSQL
   user = "your_username",
   password = "your_password"
 )
 
-# UI
+# Function to perform the database query for a given server
+perform_query <- function(server_name) {
+  conn_params <- db_params
+  conn_params$host <- server_name
+  
+  conn <- dbConnect(PostgreSQL(), dbname = conn_params$dbname, host = conn_params$host,
+                    port = conn_params$port, user = conn_params$user, password = conn_params$password)
+
+  query <- "SELECT * FROM your_table;"  # Replace with your actual query
+
+  print(paste("Executing query for server", server_name, "..."))
+  data <- dbGetQuery(conn, query)
+
+  dbDisconnect(conn)
+  print(paste("Query executed successfully for server", server_name, "."))
+  return(data)
+}
+
+
+## ui.r
+library(shiny)
+library(shinydashboard)
+
+# Define the UI
 ui <- dashboardPage(
-  dashboardHeader(title = "PostgreSQL Query App"),
+  dashboardHeader(title = "Multiple PostgreSQL Servers Query App"),
   dashboardSidebar(),
   dashboardBody(
     fluidRow(
       box(
-        title = "Query Result",
+        title = "Combined Query Result",
         status = "primary",
         solidHeader = TRUE,
-        width = 6,
-        dataTableOutput("query_result")
+        width = 12,
+        dataTableOutput("combined_result")
       )
     ),
     fluidRow(
       box(
-        title = "Send Query",
+        title = "Send Query to All Servers",
         status = "info",
         solidHeader = TRUE,
         width = 6,
@@ -37,31 +60,33 @@ ui <- dashboardPage(
   )
 )
 
-# Server
+## server.r
+# Load required libraries
+library(shiny)
+library(shinydashboard)
+
+# Include the global.R file
+source("global.R")
+
+# Define the server function
 server <- function(input, output) {
-  # Function to perform the database query
-  perform_query <- function() {
-    conn <- dbConnect(PostgreSQL(), dbname = db_params$dbname, host = db_params$host,
-                      port = db_params$port, user = db_params$user, password = db_params$password)
-    
-    query <- "SELECT * FROM your_table;"  # Replace with your actual query
-    
-    print("Executing query...")
-    data <- dbGetQuery(conn, query)
-    
-    dbDisconnect(conn)
-    print("Query executed successfully.")
-    return(data)
+  # Function to get the combined result from all servers
+  get_combined_result <- function() {
+    # Replace the server names with your actual server names
+    servers <- c("server1", "server2", "server3")
+
+    combined_data <- lapply(servers, perform_query)
+    combined_data <- do.call(rbind, combined_data)
+
+    return(combined_data)
   }
-  
+
   # Button event handler
   observeEvent(input$send_query, {
-    output$query_result <- renderDataTable({
-      data <- perform_query()
+    output$combined_result <- renderDataTable({
+      data <- get_combined_result()
       data
     })
   })
 }
 
-# Run the application
-shinyApp(ui, server)
