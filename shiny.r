85db6e6fa897ac7ec638dd0aaba462af581a99af
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-02 20:08:10 -0400

    Update shiny.r

--- shiny.r
+++ shiny.r
@@ -33,3 +33,32 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
+library(shiny)
+library(shinydashboard)
+library(htmltools)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Shiny Dashboard with R Markdown Tab"),
+  dashboardSidebar(
+    # Your sidebar content here (if any)
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard",
+        # Your dashboard content here
+        h2("This is the dashboard content")
+      ),
+      tabItem(tabName = "rmarkdown_tab",
+        # Use htmltools::tags$iframe to embed the R Markdown content
+        htmltools::tags$iframe(src = "report.html", width = "100%", height = "100%")
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  # Server logic goes here, if needed
+}
+
+shinyApp(ui, server)
