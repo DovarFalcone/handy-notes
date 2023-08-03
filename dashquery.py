efeb8856fb0165e46396e45e504160657ff57b31
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-03 15:50:50 -0400

    Update dashquery.py

--- dashquery.py
+++ dashquery.py
@@ -1,10 +1,11 @@
 pip install dash dash-html-components dash-core-components dash-table psycopg2
 
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 import psycopg2
 
 # Database connection parameters (common for all servers)
@@ -34,30 +35,33 @@ def perform_query(server_name):
     print(f"Query executed successfully for server {server_name}.")
     return data
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
     html.H1("Shiny App with Python - PostgreSQL"),
     dcc.Input(id="server-input", type="text", value="server1"),
     html.Button("Send Query", id="query-button", n_clicks=0),
+    html.Div(id="loading-output"),
     html.Div(id="output-table")
 ])
 
 @app.callback(
-    dash.dependencies.Output("output-table", "children"),
-    [dash.dependencies.Input("query-button", "n_clicks")],
-    [dash.dependencies.State("server-input", "value")]
+    [Output("output-table", "children"),
+     Output("loading-output", "children")],
+    [Input("query-button", "n_clicks")],
+    [State("server-input", "value")]
 )
 def update_table(n_clicks, server_name):
     if n_clicks > 0:
+        loading_spinner = html.Div(dbc.Spinner(color="primary"))
         data = perform_query(server_name)
         table = dash_table.DataTable(
             columns=[{"name": col, "id": col} for col in data[0]._fields],
             data=data
         )
-        return table
+        return table, loading_spinner
     else:
-        return html.Div()
+        return html.Div(), html.Div()
 
 if __name__ == "__main__":
     app.run_server(debug=True)
