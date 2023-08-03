0ed072dad28a2ee909a1a2873d2eb614ef39b907
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-02 20:29:02 -0400

    Update shiny.r

--- shiny.r
+++ shiny.r
@@ -1,63 +1,20 @@
-library(shiny)
-library(shinydashboard)
-library(rmarkdown)
-
-ui <- dashboardPage(
-  dashboardHeader(title = "Shiny Dashboard with R Markdown Tab"),
-  dashboardSidebar(
-    # Your sidebar content here (if any)
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "dashboard",
-        # Your dashboard content here
-        h2("This is the dashboard content")
-      ),
-      tabItem(tabName = "rmarkdown_tab",
-        # Use uiOutput to render the R Markdown content
-        uiOutput("rmarkdown_output")
-      )
-    )
-  )
-)
-
-server <- function(input, output) {
-  # Render the R Markdown content as Shiny UI elements
-  output$rmarkdown_output <- renderUI({
-    # Read the R Markdown file
-    rmarkdown_content <- rmarkdown::render("report.Rmd")
-
-    # Return the HTML content of the rendered R Markdown
-    HTML(rmarkdown_content)
-  })
-}
-
-shinyApp(ui, server)
-
+# Load required libraries
 library(shiny)
 library(shinydashboard)
 
+# UI part of the Shiny app
 ui <- dashboardPage(
-  dashboardHeader(title = "Shiny Dashboard with R Markdown Tab"),
-  dashboardSidebar(
-    # Your sidebar content here (if any)
-  ),
+  dashboardHeader(title = "Custom Page in ShinyDashboard"),
+  dashboardSidebar(),
   dashboardBody(
-    tabItems(
-      tabItem(tabName = "dashboard",
-        # Your dashboard content here
-        h2("This is the dashboard content")
-      ),
-      tabItem(tabName = "rmarkdown_tab",
-        # Use includeMarkdown to render the R Markdown file
-        includeMarkdown("report.Rmd")
-      )
-    )
+    # Use iframe to display the custom HTML content
+    tags$iframe(src = "custom_page.html", width = "100%", height = "100%")
   )
 )
 
+# Server part of the Shiny app (you can leave this part empty if you don't have any reactive elements)
 server <- function(input, output) {
-  # Server logic goes here, if needed
 }
 
+# Run the Shiny app
 shinyApp(ui, server)
