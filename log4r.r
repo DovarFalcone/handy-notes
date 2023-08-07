57add9d6a84db5ddbb06d7e62e9e1b4bb89469fb
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-07 10:01:58 -0400

    Update log4r.r

--- log4r.r
+++ log4r.r
@@ -1,34 +1,22 @@
-# server.R
 library(shiny)
 library(shinydashboard)
-library(log4r)
 
-# Initialize the logger
-logger <- log4r::logger()
-
-# Configure the appender to log to a file
-log_file <- file("app.log")
-appender <- log4r::fileAppender("file_appender", file = log_file)
-log4r::appender(logger) <- appender
-
-# Set log level (DEBUG, INFO, WARN, ERROR, FATAL)
-log4r::threshold(logger) <- "INFO"  # Adjust log level as needed
-
-server <- function(input, output) {
-
-  # Function to log button clicks
-  log_button_click <- function(button_name) {
-    log4r::info(logger, paste("Button clicked:", button_name))
-  }
-
-  # Observe button clicks and log them
-  observeEvent(input$count, {
-    log_button_click("Progress Button")
-  })
-  observeEvent(input$approval, {
-    log_button_click("Approval Button")
-  })
-
-  # ... Your other server logic ...
-
+# Function to log button clicks
+log_button_click <- function(button_name) {
+  message(paste("Button clicked:", button_name))
 }
+
+# Observe button clicks and log them
+observeEvent(input$count, {
+  log_button_click("Progress Button")
+})
+observeEvent(input$approval, {
+  log_button_click("Approval Button")
+})
+
+# ... Your other server logic ...
+
+# Redirect R console output to a log file
+log_file <- "app.log"
+sink(log_file, append = TRUE, split = TRUE)
+on.exit(sink(NULL))  # To close the connection when the app exits
