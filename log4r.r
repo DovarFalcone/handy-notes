7a411e1316518263138c51c8173fb512d4e5321a
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-07 10:17:22 -0400

    Update log4r.r

--- log4r.r
+++ log4r.r
@@ -1,37 +1,37 @@
 # Load required libraries
 library(shiny)
-library(logging)
+library(log4r)
 
 # Initialize the logger to log to a file
-log_appender(appender_file("app.log", layout = layout_pattern(pattern = "%d{ISO8601} %-5p [%c] %m%n")))
-log_threshold(INFO)
+log_file_appender("app.log", threshold = "INFO")
+log_threshold("INFO")
 
 # Define the Shiny server function
 server <- function(input, output, session) {
   
   # Log session information at the start of the app
-  log_info("Shiny app started.")
-  log_info("Session ID:", session$token)
-  log_info("Client address:", session$clientData$session$clientAddress)
-  log_info("User agent:", session$clientData$session$userAgent)
-  log_info("Server address:", session$clientData$url_protocol$HTTP_HOST)
-  log_info("App path:", session$clientData$url_pathname)
+  loginfo("Shiny app started.")
+  loginfo("Session ID:", session$token)
+  loginfo("Client address:", session$clientData$session$clientAddress)
+  loginfo("User agent:", session$clientData$session$userAgent)
+  loginfo("Server address:", session$clientData$url_protocol$HTTP_HOST)
+  loginfo("App path:", session$clientData$url_pathname)
   
   observe({
     # Some computation or data manipulation
-    log_info("Data manipulation completed.")
+    loginfo("Data manipulation completed.")
     # More code...
   })
   
   output$plot <- renderPlot({
     # Some plotting code
-    log_debug("Plot rendering started.")
+    logdebug("Plot rendering started.")
     # More code...
   })
   
   observeEvent(input$button, {
     # Some action taken upon button click
-    log_warn("Button clicked with input:", input$button)
+    logwarn("Button clicked with input:", input$button)
     # More code...
   })
   
@@ -39,7 +39,7 @@ server <- function(input, output, session) {
   
   # Log session information when the app is closed
   session$onSessionEnded(function() {
-    log_info("Shiny app session ended.")
+    loginfo("Shiny app session ended.")
   })
 }
 
