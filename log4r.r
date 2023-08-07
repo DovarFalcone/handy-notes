9a4ba725a3349e02712235ce13b9363bafb9a918
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-07 10:13:25 -0400

    Update log4r.r

--- log4r.r
+++ log4r.r
@@ -1,22 +1,47 @@
+# Load required libraries
 library(shiny)
-library(shinydashboard)
+library(logging)
 
-# Function to log button clicks
-log_button_click <- function(button_name) {
-  message(paste("Button clicked:", button_name))
+# Initialize the logger to log to a file
+log_appender(appender_file("app.log", layout = layout_pattern(pattern = "%d{ISO8601} %-5p [%c] %m%n")))
+log_threshold(INFO)
+
+# Define the Shiny server function
+server <- function(input, output, session) {
+  
+  # Log session information at the start of the app
+  log_info("Shiny app started.")
+  log_info("Session ID:", session$token)
+  log_info("Client address:", session$clientData$session$clientAddress)
+  log_info("User agent:", session$clientData$session$userAgent)
+  log_info("Server address:", session$clientData$url_protocol$HTTP_HOST)
+  log_info("App path:", session$clientData$url_pathname)
+  
+  observe({
+    # Some computation or data manipulation
+    log_info("Data manipulation completed.")
+    # More code...
+  })
+  
+  output$plot <- renderPlot({
+    # Some plotting code
+    log_debug("Plot rendering started.")
+    # More code...
+  })
+  
+  observeEvent(input$button, {
+    # Some action taken upon button click
+    log_warn("Button clicked with input:", input$button)
+    # More code...
+  })
+  
+  # More server code...
+  
+  # Log session information when the app is closed
+  session$onSessionEnded(function() {
+    log_info("Shiny app session ended.")
+  })
 }
 
-# Observe button clicks and log them
-observeEvent(input$count, {
-  log_button_click("Progress Button")
-})
-observeEvent(input$approval, {
-  log_button_click("Approval Button")
-})
-
-# ... Your other server logic ...
-
-# Redirect R console output to a log file
-log_file <- "app.log"
-sink(log_file, append = TRUE, split = TRUE)
-on.exit(sink(NULL))  # To close the connection when the app exits
+# Run the Shiny app
+shinyApp(ui, server)
