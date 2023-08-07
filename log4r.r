2229c903717aaee24e2c0a634e41731bcb902c71
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-07 10:19:45 -0400

    Update log4r.r

--- log4r.r
+++ log4r.r
@@ -2,36 +2,39 @@
 library(shiny)
 library(log4r)
 
+# Create a logger instance
+logger <- log4r::logger()
+
 # Initialize the logger to log to a file
-log_file_appender("app.log", threshold = "INFO")
-log_threshold("INFO")
+appender <- log4r::file_appender("app.log", append = TRUE, threshold = "INFO")
+log4r::add_appender(logger, appender)
 
 # Define the Shiny server function
 server <- function(input, output, session) {
   
   # Log session information at the start of the app
-  loginfo("Shiny app started.")
-  loginfo("Session ID:", session$token)
-  loginfo("Client address:", session$clientData$session$clientAddress)
-  loginfo("User agent:", session$clientData$session$userAgent)
-  loginfo("Server address:", session$clientData$url_protocol$HTTP_HOST)
-  loginfo("App path:", session$clientData$url_pathname)
+  log4r::loginfo(logger, "Shiny app started.")
+  log4r::loginfo(logger, "Session ID:", session$token)
+  log4r::loginfo(logger, "Client address:", session$clientData$session$clientAddress)
+  log4r::loginfo(logger, "User agent:", session$clientData$session$userAgent)
+  log4r::loginfo(logger, "Server address:", session$clientData$url_protocol$HTTP_HOST)
+  log4r::loginfo(logger, "App path:", session$clientData$url_pathname)
   
   observe({
     # Some computation or data manipulation
-    loginfo("Data manipulation completed.")
+    log4r::loginfo(logger, "Data manipulation completed.")
     # More code...
   })
   
   output$plot <- renderPlot({
     # Some plotting code
-    logdebug("Plot rendering started.")
+    log4r::logdebug(logger, "Plot rendering started.")
     # More code...
   })
   
   observeEvent(input$button, {
     # Some action taken upon button click
-    logwarn("Button clicked with input:", input$button)
+    log4r::logwarn(logger, "Button clicked with input:", input$button)
     # More code...
   })
   
@@ -39,7 +42,7 @@ server <- function(input, output, session) {
   
   # Log session information when the app is closed
   session$onSessionEnded(function() {
-    loginfo("Shiny app session ended.")
+    log4r::loginfo(logger, "Shiny app session ended.")
   })
 }
 
