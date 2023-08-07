24655f50e762d18234c35d43f01bbc62231a67b0
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2023-08-07 11:07:09 -0400

    Update log4r.r

--- log4r.r
+++ log4r.r
@@ -4,19 +4,24 @@ library(log4r)
 
 # Create a logger with a file appender and set the logging level
 logger <- log4r::logger("shiny_app")
-appender <- log4r::fileAppender("app.log", loglevel = "INFO", append = TRUE)
+appender <- log4r::file_appender("app.log", loglevel = "INFO", append = TRUE)
 log4r::add_appender(logger, appender)
 
 # Define the Shiny server function
 server <- function(input, output, session) {
   
+  # Function to get session information as a character string
+  get_session_info <- function() {
+    paste("Session ID:", session$token,
+          "Client address:", session$clientData$session$clientAddress,
+          "User agent:", session$clientData$session$userAgent,
+          "Server address:", session$clientData$url_protocol$HTTP_HOST,
+          "App path:", session$clientData$url_pathname)
+  }
+  
   # Log session information at the start of the app
   log4r::loginfo(logger, "Shiny app started.")
-  log4r::loginfo(logger, "Session ID:", session$token)
-  log4r::loginfo(logger, "Client address:", session$clientData$session$clientAddress)
-  log4r::loginfo(logger, "User agent:", session$clientData$session$userAgent)
-  log4r::loginfo(logger, "Server address:", session$clientData$url_protocol$HTTP_HOST)
-  log4r::loginfo(logger, "App path:", session$clientData$url_pathname)
+  log4r::loginfo(logger, get_session_info())
   
   observe({
     # Some computation or data manipulation
@@ -45,4 +50,3 @@ server <- function(input, output, session) {
 }
 
 # Run the Shiny app
-shinyApp(ui, server)
