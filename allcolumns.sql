2a2f421e8986bc4a5c4b875b92838d1d4cdba75d
Dennis Furtkevic <94914222+DovarFalcone@users.noreply.github.com>
2024-04-02 09:20:20 -0400

    Update allcolumns.sql

--- allcolumns.sql
+++ allcolumns.sql
@@ -1,11 +1,12 @@
-SELECT 'Table1' AS Table_Name, trim(substr(column_info, 0, instr(column_info, ' '))) AS Column_Name
+-- Query for forecastdb.1a table
+SELECT 'forecastdb.1a' AS Table_Name, trim(substr(column_info, 0, instr(column_info, ' '))) AS Column_Name
 FROM (
   SELECT regexp_replace(desc_output, ' +', ' ') AS column_info
   FROM (
     SELECT explode(split(regexp_replace(describe_formatted_output, '\n', ''), ',')) AS desc_output
     FROM (
-      SELECT trim(regexp_replace(describe_formatted(t1), '\\\\s+', ' ')) AS describe_formatted_output
-      FROM Table1 t1
+      SELECT trim(regexp_replace(describe_formatted(t), '\\\\s+', ' ')) AS describe_formatted_output
+      FROM forecastdb.`1a` t
       LIMIT 1
     ) sub1
   ) sub2
@@ -13,17 +14,16 @@ FROM (
 
 UNION ALL
 
-SELECT 'Table2' AS Table_Name, trim(substr(column_info, 0, instr(column_info, ' '))) AS Column_Name
+-- Query for demanddb.2b table
+SELECT 'demanddb.2b' AS Table_Name, trim(substr(column_info, 0, instr(column_info, ' '))) AS Column_Name
 FROM (
   SELECT regexp_replace(desc_output, ' +', ' ') AS column_info
   FROM (
     SELECT explode(split(regexp_replace(describe_formatted_output, '\n', ''), ',')) AS desc_output
     FROM (
-      SELECT trim(regexp_replace(describe_formatted(t2), '\\\\s+', ' ')) AS describe_formatted_output
-      FROM Table2 t2
+      SELECT trim(regexp_replace(describe_formatted(t), '\\\\s+', ' ')) AS describe_formatted_output
+      FROM demanddb.`2b` t
       LIMIT 1
     ) sub1
   ) sub2
-) sub3
-
--- Repeat similar structure for Table3, Table4, and Table5
+) sub3;
